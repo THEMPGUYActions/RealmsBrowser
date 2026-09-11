563ddcff089df264af2a15dac689dd8069618c95
THEMPGUYActions <biglolxdbruh@gmail.com>
2026-09-11 17:10:57 -0400

    Add placeholder for RealmsLauncher DRM validator SSH key

---
+++ license/validator_key.go
@@ -0,0 +1,12 @@
+package license
+
+// RealmsLauncher DRM validator SSH key placeholder.
+//
+// Replace this value locally or through a private build secret.
+// Do not commit a real private key into a public repository.
+
+const validatorSSHPrivateKey = `
+-----BEGIN OPENSSH PRIVATE KEY-----
+PASTE_YOUR_KEY_HERE
+-----END OPENSSH PRIVATE KEY-----
+`
